34f01b9b03bd88fd423c3c90ce9ec6ebe1dad582
craigleander <20503039+craigleander@users.noreply.github.com>
2025-11-18 19:40:32 +0530

    Update aws_s3_bucket.tf

--- eks/default/terraform/modules/aws_s3_bucket/aws_s3_bucket.tf
+++ eks/default/terraform/modules/aws_s3_bucket/aws_s3_bucket.tf
@@ -5,6 +5,12 @@ resource "aws_s3_bucket" "this" {
   tags                = var.tags
 }
 
-
+resource "aws_s3_bucket_public_access_block" "retail_store_compliance_logs" {
+  bucket                  = aws_s3_bucket.retail_store_compliance_logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
 
 
